774c4ae90a26e9194f66edf88e1135c677b74a49
eggplannt <a.oguntade11@gmail.com>
2025-06-06 00:49:56 -0600

    not usefull info

--- cmd/web/pages/projects_templ.go
+++ cmd/web/pages/projects_templ.go
@@ -47,7 +47,7 @@ func Projects() templ.Component {
 				}()
 			}
 			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "A Flutter social media app with Firebase, enabling event posting, discovery, and user profiles, with future plans for a Golang backend.")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "A Flutter social media app with Firebase, enabling event posting, discovery, and user profiles.")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -203,7 +203,7 @@ func project(name string, tools []string) templ.Component {
 					var templ_7745c5c3_Var10 string
 					templ_7745c5c3_Var10, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/pages/projects.templ`, Line: 45, Col: 10}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/pages/projects.templ`, Line: 43, Col: 10}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var10))
 					if templ_7745c5c3_Err != nil {
@@ -275,7 +275,7 @@ func project(name string, tools []string) templ.Component {
 					var templ_7745c5c3_Var13 string
 					templ_7745c5c3_Var13, templ_7745c5c3_Err = templ.JoinStringErrs(tool)
 					if templ_7745c5c3_Err != nil {
-						return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/pages/projects.templ`, Line: 55, Col: 12}
+						return templ.Error{Err: templ_7745c5c3_Err, FileName: `cmd/web/pages/projects.templ`, Line: 53, Col: 12}
 					}
 					_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var13))
 					if templ_7745c5c3_Err != nil {
